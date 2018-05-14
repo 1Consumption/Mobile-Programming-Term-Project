f72d923f37fe84d51d6e1f6a4a0ede9c98b82f77
HANSEOP-LAPTOP\HANSEOP <hanseop95@gmail.com>
2018-05-14 16:00:34 +0900

    add timer function

--- Seobang_term_project/app/src/main/java/org/androidtown/seobang_term_project/PageFragment.java
+++ Seobang_term_project/app/src/main/java/org/androidtown/seobang_term_project/PageFragment.java
@@ -1,5 +1,6 @@
 package org.androidtown.seobang_term_project;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -9,11 +10,13 @@ import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class PageFragment extends Fragment {
 
     private String mPageString;
+    TextView timer;
 
     public static PageFragment create(String pageNumber) {
         PageFragment fragment = new PageFragment();
@@ -36,10 +39,18 @@ public class PageFragment extends Fragment {
         WebView webView = rootView.findViewById(R.id.processWebView);
         webView.getSettings().setJavaScriptEnabled(true);
 
-        WebSettings settings =  webView.getSettings();
+        WebSettings settings = webView.getSettings();
         settings.setLoadWithOverviewMode(true);
         settings.setUseWideViewPort(true);
 
+        timer = rootView.findViewById(R.id.timer);
+        timer.setText("No info");
+        Button timerButton = rootView.findViewById(R.id.timerButton);
+        timerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                new CountDownTask().execute();
+            }
+        });
         if (!mPageString.substring(mPageString.indexOf("|") + 1).isEmpty())
             webView.loadUrl(mPageString.substring(mPageString.indexOf("|") + 1));
         else {
@@ -47,5 +58,38 @@ public class PageFragment extends Fragment {
         }
         webView.setWebViewClient(new WebViewClient());
         return rootView;
+    }
+
+    private class CountDownTask extends AsyncTask<Void, Integer, Void> {
+//        @Override
+//
+//        protected void onPreExecute() {
+//            timer.setText("*START*");
+//        }
+
+        @Override
+
+        protected Void doInBackground(Void... params) {
+            for (int i = 30; i >= 0; i--) {
+                try {
+                    Thread.sleep(1000);
+                    publishProgress(i); //Invokes onProgressUpdate();
+                } catch (Exception e) {
+                }
+            }
+            return null;
+        }
+
+        @Override
+
+        protected void onProgressUpdate(Integer... values) {
+            timer.setText(Integer.toString(values[0].intValue()));
+        }
+
+//        @Override
+//
+//        protected void onPostExecute(Void aVoid) {
+//            timer.setText("*DONE*");
+//        }
     }
 }
