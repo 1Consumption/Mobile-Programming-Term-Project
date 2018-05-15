152c5ec69516721107ec7ee5675a84e74804bd4c
HANSEOP_PC\hanseop <hanseop95@gmail.com>
2018-05-16 02:57:17 +0900

    debug RecipeActivity

--- Seobang_term_project/app/src/main/java/org/androidtown/seobang_term_project/RecipeSelectActivity.java
+++ Seobang_term_project/app/src/main/java/org/androidtown/seobang_term_project/RecipeSelectActivity.java
@@ -57,10 +57,12 @@ public class RecipeSelectActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, menus);
         searchRecipe.setAdapter(adapter);
 
+        mHelper = new ProductDBHelper(getApplicationContext());
+        db = mHelper.getWritableDatabase();
+
         showRecipe.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                mHelper = new ProductDBHelper(getApplicationContext());
-                db = mHelper.getWritableDatabase();
+
                 Cursor countCursor = db.rawQuery("SELECT count(*) FROM " + TABLE_NAME, null);
                 countCursor.getCount();
                 countCursor.moveToNext();
@@ -81,16 +83,11 @@ public class RecipeSelectActivity extends AppCompatActivity {
                 } else {
                     recipeOutput.setText("No Result");
                 }
-
-                cursor.close();
-                db.close();
             }
         });
 
         goToRecipe.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                mHelper = new ProductDBHelper(getApplicationContext());
-                db = mHelper.getWritableDatabase();
                 Cursor countCursor = db.rawQuery("SELECT count(*) FROM " + TABLE_NAME + " WHERE recipe_name=\"" + searchRecipe.getText().toString() + "\"", null);
                 countCursor.getCount();
                 countCursor.moveToNext();
@@ -118,8 +115,6 @@ public class RecipeSelectActivity extends AppCompatActivity {
                     String msg = "Sorry...There is no such food.";
                     Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
                 }
-
-
             }
         });
     }
