bcba5d92eb6ad750f21b7d36d029757887ec569d
son-min-wook <desepticon@naver.com>
2018-05-16 18:54:24 +0900

    Update PageFragment.java

--- Seobang_term_project/app/src/main/java/org/androidtown/seobang_term_project/ui/recipe/PageFragment.java
+++ Seobang_term_project/app/src/main/java/org/androidtown/seobang_term_project/ui/recipe/PageFragment.java
@@ -1,169 +1,199 @@
-
 package org.androidtown.seobang_term_project;
 
-
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
-import android.content.res.AssetManager;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+public class PageFragment extends Fragment {
 
-import static android.support.v4.app.NotificationCompat.*;
-
-public class RecipeActivity extends AppCompatActivity implements PageFragment.OnTimePickerSetListener {
-    private ViewPager mViewPager;
-    private PagerAdapter mPagerAdapter;
-    int mresult;
-
-    String[] RecipeProcess;
-    public static final String ROOT_DIR = "/data/data/org.androidtown.seobang_term_project/databases/";
-    public static final String DB_Name = "recipe_process.db";
-    public static final String TABLE_NAME = "recipe_process";
-    public SQLiteDatabase db;
-    public Cursor cursor;
-    ProductDBHelper mHelper;
-
-    public void onTimePickerSet(int result) {
-        mresult = result;
-        if (mresult == 0) {
-            Builder mBuilder = new Builder(RecipeActivity.this);
-            mBuilder.setContentText("서방");
-            mBuilder.setSmallIcon(R.drawable.spoon);
-            mBuilder.setContentTitle("타이머 종료!");
-            mBuilder.setDefaults(Notification.DEFAULT_ALL);
-            mBuilder.setPriority(PRIORITY_DEFAULT);
-            mBuilder.setAutoCancel(true);
-            NotificationManager mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            mNotificationManager.notify(0, mBuilder.build());
-        }
+    private String mPageString;
+    TextView hour;
+    TextView minute;
+    TextView second;
+    LinearLayout timerLayout;
+    int result =0;
+    int time = 0;
+    private OnTimePickerSetListener onTimePickerSetListener;
+    public interface  OnTimePickerSetListener{
+        void onTimePickerSet(int result);
+    }
+    public static PageFragment create(String pageContents) {
+        PageFragment fragment = new PageFragment();
+        Bundle args = new Bundle();
+        args.putString("page", pageContents);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe);
-
-        setDB(this);
-
-        Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
-        String result = bundle.getString("selectedRecipe");
-
-        mHelper = new ProductDBHelper(getApplicationContext());
-
-
-        db = mHelper.getWritableDatabase();
-
-        cursor = db.rawQuery("SELECT process,explanation,url FROM " + TABLE_NAME + " WHERE recipe_code=\"" + result + "\"", null); //쿼리문
-
-//        Cursor URLCursor=db.rawQuery("SELECT url FROM " + TABLE_NAME + " WHERE recipe_code=\"" + result + "\"", null);
-
-        String strRecipeProcess = "";
-
-        while (cursor.moveToNext()) {
-            strRecipeProcess += cursor.getString(0);
-            strRecipeProcess += "&" + cursor.getString(1) + "|";
-            strRecipeProcess += cursor.getString(2) + "#";
-        }
-
-        strRecipeProcess = strRecipeProcess.substring(0, strRecipeProcess.length() - 1);
-
-        RecipeProcess = strRecipeProcess.split("#");
-        Log.e("LENGTH", String.valueOf(RecipeProcess.length));
-
-        QuickSort sort = new QuickSort();
-
-        sort.sort(RecipeProcess, 0, RecipeProcess.length - 1);
-
-        for (int i = 0; i < RecipeProcess.length; i++)
-            Log.e("Recipe", RecipeProcess[i]);
-
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mPagerAdapter = new PagerAdapter(getSupportFragmentManager());
-        mViewPager.setAdapter(mPagerAdapter);
+        mPageString = getArguments().getString("page");
     }
-
-    private class PagerAdapter extends FragmentStatePagerAdapter {
-
-        public PagerAdapter(FragmentManager fm) {
-            super(fm);
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        if(getActivity() !=null && getActivity() instanceof OnTimePickerSetListener){
+            onTimePickerSetListener = (OnTimePickerSetListener) getActivity();
         }
-
-        @Override
-        public Fragment getItem(int position) {
-            return PageFragment.create(RecipeProcess[position]);
-        }
-
-        @Override
-        public int getCount() {
-            Log.e("getCountTest", String.valueOf(RecipeProcess.length));
-            return RecipeProcess.length;
-        }
-
     }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        String processString = mPageString.substring(mPageString.indexOf("&") + 1, mPageString.indexOf("|"));
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_page, container, false);
+        ((TextView) rootView.findViewById(R.id.pageTextView)).setText(mPageString.substring(0, mPageString.indexOf("&")));
+        ((TextView) rootView.findViewById(R.id.recipeString)).setText(processString);
+        WebView webView = rootView.findViewById(R.id.processWebView);
+        webView.getSettings().setJavaScriptEnabled(true);
 
+        WebSettings settings = webView.getSettings();
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
 
-    public static void setDB(Context ctx) {
-        File folder = new File(ROOT_DIR);
-        if (folder.exists()) {
-        } else {
-            folder.mkdirs();
-        }
-        AssetManager assetManager = ctx.getResources().getAssets();
-        File outfile = new File(ROOT_DIR + DB_Name);
-        InputStream is = null;
-        FileOutputStream fo = null;
-        long filesize = 0;
-        try {
-            is = assetManager.open(DB_Name, AssetManager.ACCESS_BUFFER);
-            filesize = is.available();
-            if (outfile.length() <= 0) {
-                byte[] tempdata = new byte[(int) filesize];
-                is.read(tempdata);
-                is.close();
-                outfile.createNewFile();
-                fo = new FileOutputStream(outfile);
-                fo.write(tempdata);
-                fo.close();
-            } else {
+        timerLayout = rootView.findViewById(R.id.timerLayout);
+        hour = rootView.findViewById(R.id.hourText);
+        minute = rootView.findViewById(R.id.minuteText);
+        second = rootView.findViewById(R.id.secondText);
+
+        if (processString.indexOf("시간") != -1 || processString.indexOf("분") != -1) {
+            double extractedTime = 0;
+            int timeIndex = 0;
+            String timeInProcess = "";
+            final Button timerStartButton = rootView.findViewById(R.id.timerStartButton);
+
+            if (processString.indexOf("시간") != -1) {
+                timerLayout.setVisibility(View.VISIBLE);
+                timerStartButton.setVisibility(View.VISIBLE);
+                int index = processString.indexOf("시간");
+
+                timeIndex = extractTime(processString, index);
+                timeInProcess = processString.substring(timeIndex, processString.indexOf("시간"));
+
+                if (timeInProcess.indexOf("~") != -1 || timeInProcess.indexOf("-") != -1) {
+                    String[] temp = null;
+                    if (timeInProcess.indexOf("~") != -1)
+                        temp = timeInProcess.split("~");
+
+                    if (timeInProcess.indexOf("-") != -1)
+                        temp = timeInProcess.split("-");
+
+                    timeInProcess = String.valueOf((Double.parseDouble(temp[0]) + Double.parseDouble(temp[1])) / 2);
+                }
+                extractedTime = Double.parseDouble(timeInProcess) * 3600;
             }
-        } catch (IOException e) {
+
+            if (processString.indexOf("분") != -1 && processString.indexOf("분") != 0) {
+                int index = processString.indexOf("분");
+                timeIndex = extractTime(processString, index);
+                char errorCheck = processString.charAt(processString.indexOf("분") - 1);
+
+                if (Character.isDigit(errorCheck) == true) {
+                    timerLayout.setVisibility(View.VISIBLE);
+                    timerStartButton.setVisibility(View.VISIBLE);
+                    timeInProcess = processString.substring(timeIndex, processString.indexOf("분"));
+
+                    if (timeInProcess.indexOf("~") != -1 || timeInProcess.indexOf("-") != -1) {
+                        String[] temp = null;
+                        if (timeInProcess.indexOf("~") != -1)
+                            temp = timeInProcess.split("~");
+
+                        if (timeInProcess.indexOf("-") != -1)
+                            temp = timeInProcess.split("-");
+                        timeInProcess = String.valueOf((Double.parseDouble(temp[0]) + Double.parseDouble(temp[1])) / 2);
+                    }
+                    extractedTime = Double.parseDouble(timeInProcess) * 60;
+                }
+            }
+
+            time = (int) extractedTime;
+
+            timerStartButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    new CountDownTask().execute();
+                    Animation anim = AnimationUtils.loadAnimation(getActivity(), R.anim.fade_out);
+                    timerStartButton.startAnimation(anim);
+                    timerStartButton.setVisibility(View.INVISIBLE);
+                }
+            });
         }
+
+
+        if (!mPageString.substring(mPageString.indexOf("|") + 1).isEmpty())
+            webView.loadUrl(mPageString.substring(mPageString.indexOf("|") + 1));
+        else {
+            webView.loadUrl("https://github.com/HanseopShin/Mobile-Programming-Term-Project/blob/master/no_Info.png?raw=true");
+        }
+
+        webView.setWebViewClient(new WebViewClient());
+
+        return rootView;
     }
 
-    class ProductDBHelper extends SQLiteOpenHelper {  //새로 생성한 adapter 속성은 SQLiteOpenHelper이다.
-        public ProductDBHelper(Context context) {
-            super(context, DB_Name, null, 1);    // db명과 버전만 정의 한다.
-            // TODO Auto-generated constructor stub
+    private class CountDownTask extends AsyncTask<Void, Integer, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            for (int i = time; i >= 0; i--) {
+                try {
+                    Thread.sleep(1000);
+                    publishProgress(i);
+
+                } catch (Exception e) {
+                    Log.d(this.getClass().getName(), "error-exception");
+                }
+                if (i == 0) {
+                    if(onTimePickerSetListener != null){
+                        onTimePickerSetListener.onTimePickerSet(result);
+                    }
+                    else
+                        Log.d(this.getClass().getName(), "error-i is not 0");
+
+                }
+            }
+            return null;
         }
 
         @Override
-        public void onCreate(SQLiteDatabase db) {
-            // TODO Auto-generated method stub
-        }
+        protected void onProgressUpdate(Integer... values) {
+            int temp = values[0].intValue();
+            int intHour = temp / 3600;
+            int intMinute = (temp - (intHour * 3600)) / 60;
+            int intSecond = (temp - (intHour * 3600 + intMinute * 60));
 
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            // TODO Auto-generated method stub
+            hour.setText(String.format("%02d", intHour));
+            minute.setText(String.format("%02d", intMinute));
+            second.setText(String.format("%02d", intSecond));
         }
     }
 
+    private int extractTime(String process, int curIndex) {
+        int timeIndex = 0;
+
+        if (curIndex == 0)
+            return 0;
+
+        char temp = process.charAt(--curIndex);
+
+        if (Character.isDigit(temp) == true) {
+            timeIndex = extractTime(process, curIndex);
+        } else if (temp == '~' || temp == '-') {
+            timeIndex = extractTime(process, curIndex);
+        } else {
+            return curIndex + 1;
+        }
+
+        return timeIndex;
+    }
 }
