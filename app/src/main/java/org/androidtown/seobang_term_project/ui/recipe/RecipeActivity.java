1951fc0822c059c4d20eea133d496b2a8709646d
hayeon0824 <ha_eon0824@naver.com>
2018-05-23 21:16:13 +0900

    commit test

--- app/src/main/java/org/androidtown/seobang_term_project/ui/recipe/RecipeActivity.java
+++ app/src/main/java/org/androidtown/seobang_term_project/ui/recipe/RecipeActivity.java
@@ -20,8 +20,7 @@ import butterknife.BindView;
 
 public class RecipeActivity extends BaseActivity {
 
-    protected @BindView(R.id.pager)
-    ViewPager viewpager;
+    protected @BindView(R.id.pager) ViewPager viewpager;
     private PagerAdapter mPagerAdapter;
 
     private String[] RecipeProcess;
