27124e3cf4d3e0006c18f420e07929fe716b5cb1
hayeon0824 <ha_eon0824@naver.com>
2018-05-30 03:19:18 +0900

    history tab added

--- app/src/main/java/org/androidtown/seobang_term_project/ui/history/HistoryOneFragment.java
+++ app/src/main/java/org/androidtown/seobang_term_project/ui/history/HistoryOneFragment.java
@@ -1,25 +1,16 @@
 package org.androidtown.seobang_term_project.ui.history;
 
-import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.androidtown.seobang_term_project.R;
 
-public class HistoryOneFragment extends Fragment {
-
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-    }
-
+public class HistoryOneFragment extends android.support.v4.app.Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.history_one, container, false);
 
 
---
+++ app/src/main/java/org/androidtown/seobang_term_project/ui/history/TabPagerAdapter.java
@@ -0,0 +1,36 @@
+package org.androidtown.seobang_term_project.ui.history;
+
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+
+public class  TabPagerAdapter extends FragmentPagerAdapter {
+
+    private int tabCount;
+    Fragment cur_fragment;
+
+    public TabPagerAdapter(FragmentManager fm, int tabCount) {
+        super(fm);
+        this.tabCount = tabCount;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                cur_fragment = new HistoryOneFragment();
+                break;
+            case 1:
+                cur_fragment = new HistoryTwoFragment();
+                break;
+        }
+
+        return cur_fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
