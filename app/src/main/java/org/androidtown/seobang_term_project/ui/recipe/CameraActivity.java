054e2c35f64122f8155cdb61e80520c74185629b
son-min-wook <desepticon@naver.com>
2018-06-03 10:35:05 +0900

    Update CameraActivity.java

--- app/src/main/java/org/androidtown/seobang_term_project/ui/recipe/CameraActivity.java
+++ app/src/main/java/org/androidtown/seobang_term_project/ui/recipe/CameraActivity.java
@@ -1,15 +1,41 @@
 package org.androidtown.seobang_term_project.ui.recipe;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import org.androidtown.seobang_term_project.R;
 
-public class CameraActivity extends AppCompatActivity {
+public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn = null;
+    ImageView iv = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        btn = findViewById(R.id.btn1);
+        iv = findViewById(R.id.iv);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ((ImageView) findViewById(R.id.iv)).setImageBitmap(imageBitmap);
+        }
     }
 }
